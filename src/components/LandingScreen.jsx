53aeed74f5b93c79e835810659a92e1f143e1d85
Hrithik-GV <hrithikgv5@gmail.com>
2026-03-02 19:02:59 +0530

    Initial commit: Project structure and Landing Screen

---
+++ src/components/LandingScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LandingScreen.css';
+
+const LandingScreen = () => {
+    return (
+        <div className="landing-container">
+            <div className="landing-content">
+                <h1 className="landing-message">
+                    Feeling tense? <br />
+                    <span>Place your thumb anywhere on the screen.</span>
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default LandingScreen;
